0cb142d8e47c88e7e3278c5c7bc95e1b88a37675
rytkhs <jag29beem@gmail.com>
2024-05-06 23:46:13 +0900

    chore: スマホから開発サーバーに接続できなかったため設定を変更

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0', // すべてのネットワークインターフェースをリッスン
+        // port: 3000, // 使用するポート（デフォルトは3000ですが、必要に応じて変更してください）
+        hmr: {
+            host: '192.168.0.137', // ここにPCのローカルIPアドレスを指定
+        }
+      },
     plugins: [
         laravel({
             input: [
